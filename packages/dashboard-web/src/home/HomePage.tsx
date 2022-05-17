89abeeecc1264171b41c7b02f1b2517dda556b7a
Junho Yeo <i@junho.io>
2022-05-18 05:56:01 +0900

    [dashboard-web] Short wallets by allocation ratio

--- packages/dashboard-web/src/home/HomePage.tsx
+++ packages/dashboard-web/src/home/HomePage.tsx
@@ -3,8 +3,7 @@ import { WalletBalance as TendermintWalletBalance } from '@/pages/api/tendermint
 import { useAxiosSWR } from '@/hooks/useAxiosSWR';
 import { wallets } from '@dashboard/core/lib/config';
 import React, { useMemo } from 'react';
-import { TokenIcon } from './components/TokenIcon';
-import { shortenAddress } from '@dashboard/core/lib/utils';
+import { TokenBalanceItem } from './components/TokenBalanceItem';
 
 const walletBalanceReducer =
   (symbol: string, callback: (acc: number, balance: WalletBalance) => number) =>
@@ -210,64 +209,7 @@ const LandingPage = () => {
 
         <ul className="mt-8">
           {tokenBalances.map((info) => (
-            <li
-              key={info.name}
-              className="mb-2 border border-slate-700 rounded-md drop-shadow-2xl bg-slate-800/25 backdrop-blur-md flex flex-col"
-            >
-              <div className="pt-2 pb-1 px-3 flex items-center">
-                <TokenIcon src={info.logo} alt={info.name} />
-                <div className="ml-4 flex flex-col">
-                  <span className="text-md">
-                    <span className="text-slate-400">{info.symbol}</span>
-                    <span className="ml-1 text-slate-400/40">
-                      {info.amount.toLocaleString()}
-                    </span>
-                  </span>
-                  <span className="text-2xl font-bold text-slate-50/90">
-                    {`$${info.netWorth.toLocaleString()}`}
-                  </span>
-                </div>
-              </div>
-
-              <ul className="pb-2 px-3 flex flex-col">
-                {info.balances.map(
-                  (balance: WalletBalance | TendermintWalletBalance) => (
-                    <li key={balance.symbol} className="flex align-top">
-                      <span className="text-sm font-bold text-slate-400/90 min-w-[64px]">{`${(
-                        ((balance.balance +
-                          ('delegations' in balance
-                            ? balance.delegations
-                            : 0)) /
-                          info.amount) *
-                        100
-                      )
-                        .toFixed(2)
-                        .toLocaleString()}%`}</span>
-
-                      <span className="ml-2 text-sm text-slate-100/40 inline-flex min-w-[100px]">
-                        {shortenAddress(balance.walletAddress ?? '')}
-                      </span>
-
-                      <div className="ml-4 text-sm text-slate-200/70 flex flex-col items-start">
-                        <span>
-                          <span className="text-xs">Wallet</span>
-                          &nbsp;
-                          {balance.balance.toLocaleString()}
-                        </span>
-
-                        {'delegations' in balance && (
-                          <span>
-                            <span className="text-xs">Staking</span>
-                            &nbsp;
-                            {balance.delegations.toLocaleString()}
-                          </span>
-                        )}
-                      </div>
-                    </li>
-                  ),
-                )}
-              </ul>
-            </li>
+            <TokenBalanceItem {...info} />
           ))}
         </ul>
       </div>
