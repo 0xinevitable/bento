eb167a8fd1297ac4b634c42055529541bcc0a6dd
Junho Yeo <i@junho.io>
2022-05-18 05:17:36 +0900

    [dashboard-core] Implement util `shortenAddress`

--- packages/dashboard-core/src/utils.ts
+++ packages/dashboard-core/src/utils.ts
@@ -2,3 +2,9 @@ export const safePromiseAll = async <T>(promises: Promise<T>[]) =>
   (await Promise.allSettled(promises)).flatMap((res) =>
     res.status === 'fulfilled' ? res.value : [],
   );
+
+export const shortenAddress = (address: string, size: number = 4) => {
+  return `${address.substring(0, 2 + size)}...${address.substring(
+    address.length - size,
+  )}`;
+};
