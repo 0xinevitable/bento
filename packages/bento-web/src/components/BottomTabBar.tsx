9dfc139d427cb0336a422d34499c8dbdadd8a40e
Junho Yeo <i@junho.io>
2022-05-28 10:27:59 +0900

    Implement `passHref` in Links

--- packages/bento-web/src/components/BottomTabBar.tsx
+++ packages/bento-web/src/components/BottomTabBar.tsx
@@ -5,11 +5,15 @@ export const BottomTabBar = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 h-[72px] w-full">
       <div className="w-full h-full max-w-2xl mx-auto flex relative bg-slate-900/50 backdrop-blur-md drop-shadow-2xl border-t border-t-slate-800">
-        <Link href="/">
-          <button className="text-white w-[50%]">Dashboard</button>
+        <Link href="/" passHref>
+          <a className="w-[50%]">
+            <button className="text-white">Dashboard</button>
+          </a>
         </Link>
-        <Link href="/profile">
-          <button className="text-white w-[50%]">Profile</button>
+        <Link href="/profile" passHref>
+          <a className="w-[50%]">
+            <button className="text-white">Profile</button>
+          </a>
         </Link>
       </div>
     </div>
