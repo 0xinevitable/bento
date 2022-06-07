4833fcc06aca5b022ce8f6db07af823f02192a09
Junho Yeo <i@junho.io>
2022-06-08 08:45:29 +0900

    Generate signature with Web3Modal

--- packages/bento-web/src/onboarding/OnboardingPage.tsx
+++ packages/bento-web/src/onboarding/OnboardingPage.tsx
@@ -1,5 +1,8 @@
+import { Web3Provider } from '@ethersproject/providers';
+import WalletConnectProvider from '@walletconnect/web3-provider';
 import { useCallback } from 'react';
 import styled from 'styled-components';
+import Web3Modal from 'web3modal';
 
 import { PageContainer } from '@/components/PageContainer';
 import { FieldInput } from '@/profile/components/FieldInput';
@@ -10,7 +13,51 @@ declare global {
   }
 }
 
+const providerOptions = {
+  injected: {
+    display: {
+      name: 'Metamask',
+      description: 'Connect with MetaMask',
+    },
+    package: null,
+  },
+  walletconnect: {
+    display: {
+      name: 'WalletConnect',
+      description: 'Connect with WalletConnect',
+    },
+    package: WalletConnectProvider,
+    options: {
+      infuraId: 'fcb656a7b4d14c9f9b0803a5d7475877',
+    },
+  },
+};
+
 const OnboardingPage: React.FC = () => {
+  const connectMetaMask = useCallback(async () => {
+    const web3Modal = new Web3Modal({
+      network: 'mainnet',
+      cacheProvider: true,
+      providerOptions,
+      theme: 'dark',
+    });
+
+    web3Modal.clearCachedProvider();
+
+    try {
+      const instance = await web3Modal.connect();
+      const provider = new Web3Provider(instance);
+
+      const signer = provider.getSigner();
+      const account = await signer.getAddress();
+      const nonce = 'waka';
+      const signature = await signer.signMessage(nonce);
+      console.log({ account, signature });
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   const connectKeplr = useCallback(async () => {
     if (typeof window.keplr === 'undefined') {
       window.alert('Please install keplr extension');
@@ -33,8 +80,11 @@ const OnboardingPage: React.FC = () => {
       <FieldInput field="Username" />
 
       <div className="flex gap-2">
-        <Button className="p-4 text-slate-800 font-bold bg-slate-300">
-          MetaMask
+        <Button
+          className="p-4 text-slate-800 font-bold bg-slate-300"
+          onClick={connectMetaMask}
+        >
+          MetaMask or WalletConnect
         </Button>
 
         <Button className="p-4 text-slate-800 font-bold bg-slate-300">
