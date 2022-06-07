acd61a1bfa52a5c6be18a1768330ae8e916546a5
Junho Yeo <i@junho.io>
2022-06-08 07:45:30 +0900

    [bento-web] Initialize empty onboarding page

---
+++ packages/bento-web/src/onboarding/OnboardingPage.tsx
@@ -0,0 +1,7 @@
+import { PageContainer } from '@/components/PageContainer';
+
+const OnboardingPage: React.FC = () => {
+  return <PageContainer></PageContainer>;
+};
+
+export default OnboardingPage;
