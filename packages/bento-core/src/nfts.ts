d06a6280e1ef730c9123e10654672efae4b1f8ec
Junho Yeo <i@junho.io>
2022-06-09 11:38:51 +0900

    [bento-core] Implement `fetchOpenSeaAssets`

---
+++ packages/bento-core/src/nfts.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import QueryString from 'query-string';
+
+export const OPENSEA_BASE_URL = 'https://api.opensea.io/api';
+
+export type OpenSeaAsset = {
+  id: number;
+  token_id: string;
+  num_sales: number;
+  background_color: null;
+  image_url: string;
+  image_preview_url: string;
+  name: string;
+  description: string;
+};
+
+export type GetOpenSeaAssetsParams = {
+  owner: string;
+};
+
+// https://github.com/linkyvc/frontend/pull/52
+export const fetchOpenSeaAssets = async ({ owner }: GetOpenSeaAssetsParams) => {
+  const url = `${OPENSEA_BASE_URL}/v1/assets`;
+  const {
+    data: { assets },
+  } = await axios.get<{ assets: OpenSeaAsset[] }>(
+    QueryString.stringifyUrl({ url, query: { owner } }),
+    {
+      headers: {
+        'X-API-KEY': 'bea970cbbdae445a9f01b827f9ac227e',
+      },
+    },
+  );
+  return assets;
+};
