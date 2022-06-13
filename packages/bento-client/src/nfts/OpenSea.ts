44f8efe8d322960e7f3e51a89bf9266c71365393
Junho Yeo <i@junho.io>
2022-06-14 04:00:31 +0900

    [bento-client] Implement `OpenSea.getCollectionStats`

--- packages/bento-client/src/nfts/OpenSea.ts
+++ packages/bento-client/src/nfts/OpenSea.ts
@@ -27,17 +27,13 @@ export type GetOpenSeaAssetsParams = {
   owner: string;
   cursor?: string;
 };
-
 export type OpenSeaAssetsResponse = {
   assets: OpenSeaAsset[];
   next: string;
 };
 
 // https://github.com/linkyvc/frontend/pull/52
-export const fetchOpenSeaAssets = async ({
-  owner,
-  cursor,
-}: GetOpenSeaAssetsParams) => {
+const getAssets = async ({ owner, cursor }: GetOpenSeaAssetsParams) => {
   const url = `${OPENSEA_BASE_URL}/v1/assets`;
   const {
     data: { assets, next },
@@ -54,4 +50,42 @@ export const fetchOpenSeaAssets = async ({
     assets,
     cursor: next,
   };
+};
+
+export type OpenSeaCollectionStatsResponse = {
+  stats: {
+    one_day_volume: number;
+    one_day_change: number;
+    one_day_sales: number;
+    one_day_average_price: number;
+    seven_day_volume: number;
+    seven_day_change: number;
+    seven_day_sales: number;
+    seven_day_average_price: number;
+    thirty_day_volume: number;
+    thirty_day_change: number;
+    thirty_day_sales: number;
+    thirty_day_average_price: number;
+    total_volume: number;
+    total_sales: number;
+    total_supply: number;
+    count: number;
+    num_owners: number;
+    average_price: number;
+    num_reports: number;
+    market_cap: number;
+    floor_price: number;
+  };
+};
+
+const getCollectionStats = async (slug: string) => {
+  const { data } = await cachedAxios.get<OpenSeaCollectionStatsResponse>(
+    `https://api.opensea.io/api/v1/collection/${slug}/stats`,
+  );
+  return data;
+};
+
+export const OpenSea = {
+  getAssets,
+  getCollectionStats,
 };
