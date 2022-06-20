c37bc08b7d93b31f1fbd5034d2f05c10ce804443
Junho Yeo <i@junho.io>
2022-06-21 04:35:55 +0900

    Set cookie after auth state change

--- packages/bento-web/src/hooks/useSession.ts
+++ packages/bento-web/src/hooks/useSession.ts
@@ -1,8 +1,24 @@
+import { AuthChangeEvent, Session } from '@supabase/supabase-js';
 import { useCallback, useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 
 import { sessionAtom } from '@/recoil/session';
 import { Supabase } from '@/utils/Supabase';
+
+const handleAuthChange = async (
+  event: AuthChangeEvent,
+  session: Session | null,
+) => {
+  if (!session) {
+    return;
+  }
+  await fetch('/api/auth', {
+    method: 'POST',
+    headers: new Headers({ 'Content-Type': 'application/json' }),
+    credentials: 'same-origin',
+    body: JSON.stringify({ event, session }),
+  });
+};
 
 export const SessionManager: React.FC = () => {
   const [currentSession, setCurrentSession] = useRecoilState(sessionAtom);
@@ -11,6 +27,8 @@ export const SessionManager: React.FC = () => {
     setCurrentSession(Supabase.auth.session());
 
     Supabase.auth.onAuthStateChange((event, session) => {
+      handleAuthChange(event, session);
+
       if (event == 'SIGNED_IN') {
         if (currentSession?.user?.id !== session?.user?.id) {
           setCurrentSession(session);
