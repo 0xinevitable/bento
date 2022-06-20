2ce64f77a1569da8e587330e0fd66719e4f2fbff
Junho Yeo <i@junho.io>
2022-06-21 04:07:56 +0900

    Show sign out button instead of `LoginNudge` when logged in

--- packages/bento-web/src/settings/SettingsPage.tsx
+++ packages/bento-web/src/settings/SettingsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { PageContainer } from '@/components/PageContainer';
+import { useSession } from '@/hooks/useSession';
 import { LoginNudge } from '@/profile/components/LoginNudge';
 
 // import { Analytics } from '@/utils/analytics';
@@ -10,9 +11,18 @@ const SettingsPage = () => {
   //   Analytics.logEvent('view_settings');
   // }, []);
 
+  const { session, signOut } = useSession();
+
+  const isLoggedIn = !!session;
+
   return (
     <PageContainer>
-      <LoginNudge />
+      {!isLoggedIn && <LoginNudge />}
+      {isLoggedIn && (
+        <button className="py-4 px-8 bg-gray-300 " onClick={signOut}>
+          Sign Out
+        </button>
+      )}
     </PageContainer>
   );
 };
