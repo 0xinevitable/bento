8641d4a917f8a7c27a19352b3d10ccb4337f2e2d
Junho Yeo <i@junho.io>
2022-06-28 03:40:21 +0900

    Add logo to navigation bar

--- packages/bento-landing/src/components/NavigationBar.tsx
+++ packages/bento-landing/src/components/NavigationBar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -5,7 +6,14 @@ export const NavigationBar = () => {
   return (
     <Wrapper>
       <Container>
-        <HiddenTitle>Bento</HiddenTitle>
+        <Link href="/" passHref>
+          <a>
+            <HiddenTitle>Bento</HiddenTitle>
+            <LogoWrapper>
+              <LogoImage src="/assets/illusts/bento-logo-with-blur.png" />
+            </LogoWrapper>
+          </a>
+        </Link>
       </Container>
     </Wrapper>
   );
@@ -14,12 +22,15 @@ export const NavigationBar = () => {
 const Wrapper = styled.header`
   width: 100%;
   height: 64px;
+  padding: 0 20px;
 
   border-bottom: 1px solid #323232;
   background-color: rgba(0, 0, 0, 0.35);
   backdrop-filter: blur(12px);
 
   display: flex;
+  justify-content: center;
+
   position: fixed;
   top: 0;
   left: 0;
@@ -37,4 +48,15 @@ const Container = styled.div`
 `;
 const HiddenTitle = styled.span`
   display: none;
+`;
+const LogoWrapper = styled.div`
+  width: 156px;
+  height: 78px;
+`;
+
+const BLUR_SIZE = 31.2;
+const LogoImage = styled.img`
+  margin: ${-BLUR_SIZE}px;
+  width: ${156 + BLUR_SIZE * 2}px;
+  height: ${78 + BLUR_SIZE * 2}px;
 `;
