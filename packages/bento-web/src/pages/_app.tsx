a69e83587c3fb3e06b565e7ca00eff18f0fe9a72
Junho Yeo <i@junho.io>
2022-07-07 11:37:18 +0900

    Move outermost container & GNB to `_app`

--- packages/bento-web/src/pages/_app.tsx
+++ packages/bento-web/src/pages/_app.tsx
@@ -7,7 +7,9 @@ import '@/styles/tailwind.css';
 import '@/styles/global.css';
 
 import { AppProps } from 'next/app';
+import styled from 'styled-components';
 
+import { NavigationBar } from '@/components/NavigationBar';
 import { SessionManager } from '@/hooks/useSession';
 import { WalletManager } from '@/hooks/useWallets';
 
@@ -26,10 +28,34 @@ function MyApp({ Component, pageProps }: MyAppProps) {
       <ReactNotifications />
       <SessionManager />
       <WalletManager />
-      <Component {...pageProps} />
+
+      <Container>
+        <NavigationBar />
+        <Black />
+
+        <Component {...pageProps} />
+      </Container>
+
       <div id="portal" />
     </RecoilRoot>
   );
 }
 
 export default MyApp;
+
+const Container = styled.div`
+  width: 100vw;
+  padding-bottom: 100px;
+
+  position: relative;
+  overflow: hidden;
+
+  display: flex;
+  flex-direction: column;
+  background: #0a0a0c;
+`;
+const Black = styled.div`
+  width: 100%;
+  height: 64px;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
