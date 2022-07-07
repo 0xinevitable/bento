b42a9deb36ceb0577a9f67b2a710110efefc2ec3
Junho Yeo <i@junho.io>
2022-07-08 04:43:11 +0900

    Fix typo: `cosmosHub` -> `cosmos-hub`

--- packages/bento-web/src/dashboard/constants/platform.ts
+++ packages/bento-web/src/dashboard/constants/platform.ts
@@ -4,7 +4,7 @@ export const PLATFORM_LOGOS = {
   bnb: 'https://assets-cdn.trustwallet.com/blockchains/binance/info/logo.png',
   polygon: '/assets/polygon.webp',
   klaytn: 'https://avatars.githubusercontent.com/u/41137100?s=200&v=4',
-  cosmosHub:
+  'cosmos-hub':
     'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/cosmos/info/logo.png',
   osmosis:
     'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/osmosis/info/logo.png',
