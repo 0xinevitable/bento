dea3a160aebf1f251b0f8df2bb7a4bd630605e93
Junho Yeo <i@junho.io>
2022-07-08 06:01:45 +0900

    Update design for `AssetRatioListItem`

--- packages/bento-web/src/dashboard/DashboardPage.tsx
+++ packages/bento-web/src/dashboard/DashboardPage.tsx
@@ -14,11 +14,11 @@ import { walletsAtom } from '@/recoil/wallets';
 
 import { AddWalletModal } from './components/AddWalletModal';
 import { AssetRatioChart } from './components/AssetRatioChart';
+import { AssetRatioListItem } from './components/AssetRatioListItem';
 import { EmptyBalance } from './components/EmptyBalance';
 import { EmptyWallet } from './components/EmptyWallet';
 import { TokenBalanceItem } from './components/TokenBalanceItem';
 import { WalletList } from './components/WalletList';
-import { PLATFORM_LOGOS } from './constants/platform';
 import {
   CosmosSDKWalletBalance,
   EVMWalletBalance,
@@ -334,20 +334,7 @@ const DashboardPage = () => {
             </div>
             <AssetCardList className="w-1/2">
               {assetRatioByPlatform.map((item) => (
-                <AssetRatioCard>
-                  <img
-                    className="w-14 h-14 rounded-full"
-                    src={
-                      PLATFORM_LOGOS[
-                        item.platform as keyof typeof PLATFORM_LOGOS
-                      ]
-                    }
-                    alt={item.name}
-                  />
-                  {item.name}
-                  {`$${item.netWorth.toLocaleString()}`}
-                  {`${item.ratio}%`}
-                </AssetRatioCard>
+                <AssetRatioListItem {...item} />
               ))}
               {/* TODO: net worth & asset ratio breakdown by chains here */}
             </AssetCardList>
@@ -463,26 +450,18 @@ const CardTitle = styled.h2`
 
 const AssetCardList = styled.ul`
   margin: 0;
-  padding: 0;
-`;
-const AssetRatioCard = styled.li`
+  margin-left: 20px;
+  padding: 10px 12px;
   width: 100%;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 
   background: #16181a;
   background: linear-gradient(145deg, #141617, #181a1c);
   border: 1px solid #2a2e31;
   box-shadow: inset 5px 5px 16px #0b0c0e, inset -5px -5px 16px #212426;
   border-radius: 8px;
-
-  display: flex;
-  align-items: center;
-  padding: 8px 12px;
-
-  @media screen and (max-width: 797px) {
-    width: calc(50% - 4px);
-  }
-
-  @media screen and (max-width: 537px) {
-    width: 100%;
-  }
 `;
