3c429b5363529e62a145647dad3a996b8cd1901e
Junho Yeo <i@junho.io>
2022-07-13 00:34:24 +0900

    [web] Refactor duplicated logic with `useWalletBalances`

--- packages/bento-web/src/dashboard/DashboardPage.tsx
+++ packages/bento-web/src/dashboard/DashboardPage.tsx
@@ -9,7 +9,6 @@ import styled from 'styled-components';
 
 import { NoSSR } from '@/components/NoSSR';
 import { PageContainer } from '@/components/PageContainer';
-import { useAxiosSWR } from '@/hooks/useAxiosSWR';
 import { walletsAtom } from '@/recoil/wallets';
 
 import { AddWalletModal } from './components/AddWalletModal';
@@ -20,13 +19,8 @@ import { EmptyWallet } from './components/EmptyWallet';
 import { TokenBalanceItem } from './components/TokenBalanceItem';
 import { WalletList } from './components/WalletList';
 import { displayName } from './constants/platform';
-import {
-  CosmosSDKWalletBalance,
-  EVMWalletBalance,
-  NFTWalletBalance,
-  SolanaWalletBalance,
-  WalletBalance,
-} from './types/balance';
+import { NFTWalletBalance, WalletBalance } from './types/balance';
+import { useWalletBalances } from './utils/useWalletBalances';
 
 const walletBalanceReducer =
   (symbol: string, callback: (acc: number, balance: WalletBalance) => number) =>
@@ -35,105 +29,7 @@ const walletBalanceReducer =
 
 const DashboardPage = () => {
   const wallets = useRecoilValue(walletsAtom);
-
-  const [
-    cosmosWalletQuery,
-    ethereumWalletQuery,
-    bnbWalletQuery,
-    avalancheWalletQuery,
-    polygonWalletQuery,
-    klaytnWalletQuery,
-    solanaWalletQuery,
-  ] = useMemo(() => {
-    const addrs = wallets.reduce<
-      Record<
-        | 'cosmos'
-        | 'ethereum'
-        | 'bnb'
-        | 'avalanche'
-        | 'polygon'
-        | 'klaytn'
-        | 'solana',
-        string[]
-      >
-    >(
-      (acc, wallet) => {
-        if (wallet.type === 'cosmos-sdk') {
-          return { ...acc, cosmos: [...acc.cosmos, wallet.address] };
-        }
-        if (wallet.type === 'solana') {
-          return { ...acc, solana: [...acc.solana, wallet.address] };
-        }
-        if (wallet.type !== 'evm') {
-          return acc;
-        }
-
-        let _acc = acc;
-        if (wallet.networks.includes('ethereum')) {
-          _acc = { ..._acc, ethereum: [..._acc.ethereum, wallet.address] };
-        }
-        if (wallet.networks.includes('bnb')) {
-          _acc = { ..._acc, bnb: [..._acc.bnb, wallet.address] };
-        }
-        if (wallet.networks.includes('avalanche')) {
-          _acc = { ..._acc, avalanche: [..._acc.avalanche, wallet.address] };
-        }
-        if (wallet.networks.includes('polygon')) {
-          _acc = { ..._acc, polygon: [..._acc.polygon, wallet.address] };
-        }
-        if (wallet.networks.includes('klaytn')) {
-          _acc = { ..._acc, klaytn: [..._acc.klaytn, wallet.address] };
-        }
-        return _acc;
-      },
-      {
-        cosmos: [],
-        ethereum: [],
-        bnb: [],
-        avalanche: [],
-        polygon: [],
-        klaytn: [],
-        solana: [],
-      },
-    );
-
-    return [
-      addrs.cosmos.join(','),
-      addrs.ethereum.join(','),
-      addrs.bnb.join(','),
-      addrs.avalanche.join(','),
-      addrs.polygon.join(','),
-      addrs.klaytn.join(','),
-      addrs.solana.join(','),
-    ];
-  }, [wallets]);
-
-  const { data: ethereumBalance = [] } = useAxiosSWR<EVMWalletBalance[]>(
-    !ethereumWalletQuery ? null : `/api/evm/ethereum/${ethereumWalletQuery}`,
-  );
-  const { data: bnbBalance = [] } = useAxiosSWR<EVMWalletBalance[]>(
-    !bnbWalletQuery ? null : `/api/evm/bnb/${bnbWalletQuery}`,
-  );
-  const { data: avalancheBalance = [] } = useAxiosSWR<EVMWalletBalance[]>(
-    !avalancheWalletQuery ? null : `/api/evm/avalanche/${avalancheWalletQuery}`,
-  );
-  const { data: polygonBalance = [] } = useAxiosSWR<CosmosSDKWalletBalance[]>(
-    !polygonWalletQuery ? null : `/api/evm/polygon/${polygonWalletQuery}`,
-  );
-  const { data: klaytnBalance = [] } = useAxiosSWR<EVMWalletBalance[]>(
-    !klaytnWalletQuery ? null : `/api/evm/klaytn/${klaytnWalletQuery}`,
-  );
-  const { data: cosmosHubBalance = [] } = useAxiosSWR<CosmosSDKWalletBalance[]>(
-    !cosmosWalletQuery
-      ? null
-      : `/api/cosmos-sdk/cosmos-hub/${cosmosWalletQuery}`,
-  );
-  const { data: osmosisBalance = [] } = useAxiosSWR<CosmosSDKWalletBalance[]>(
-    !cosmosWalletQuery ? null : `/api/cosmos-sdk/osmosis/${cosmosWalletQuery}`,
-  );
-  const { data: solanaBalance = [] } = useAxiosSWR<SolanaWalletBalance[]>(
-    !solanaWalletQuery ? null : `/api/solana/mainnet/${solanaWalletQuery}`,
-  );
+  const { balances: walletBalances } = useWalletBalances({ wallets });
 
   const [NFTBalance, setNFTBalance] = useState<NFTWalletBalance[]>([]);
   const [ethereumPrice, setEthereumPrice] = useState<number>(0);
@@ -246,17 +142,7 @@ const DashboardPage = () => {
     // TODO: 추후 `tokenAddress` 로만 그룹핑 해야 할 것 같다(같은 심볼과 이름을 사용하는 토큰이 여러개 있을 수 있기 때문).
     const balancesByPlatform = Object.entries(
       groupBy<WalletBalance>(
-        [
-          ethereumBalance,
-          avalancheBalance,
-          bnbBalance,
-          polygonBalance,
-          klaytnBalance,
-          cosmosHubBalance,
-          osmosisBalance,
-          solanaBalance,
-          NFTBalance,
-        ].flat(),
+        [...walletBalances, ...NFTBalance],
         (balance) => balance.symbol + balance.name,
       ),
     ).map((v) => v[1]);
@@ -302,16 +188,7 @@ const DashboardPage = () => {
 
     tokens.sort((a, b) => b.netWorth - a.netWorth);
     return tokens.filter((v) => v.netWorth > 0);
-  }, [
-    ethereumBalance,
-    bnbBalance,
-    polygonBalance,
-    klaytnBalance,
-    cosmosHubBalance,
-    osmosisBalance,
-    solanaBalance,
-    NFTBalance,
-  ]);
+  }, [walletBalances, NFTBalance]);
 
   const netWorthInUSD = useMemo(
     () => tokenBalances.reduce((acc, info) => acc + info.netWorth, 0),
