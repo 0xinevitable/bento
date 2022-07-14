67a94ab8aa20ce788972d4e11d9141b309beeb4f
Junho Yeo <i@junho.io>
2022-07-14 17:02:06 +0900

    [web] Remove bottom margin

--- packages/bento-web/src/components/PageContainer.tsx
+++ packages/bento-web/src/components/PageContainer.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 type PageContainerProps = React.HTMLAttributes<HTMLDivElement>;
 
 export const PageContainer: React.FC<PageContainerProps> = ({
@@ -6,13 +8,15 @@ export const PageContainer: React.FC<PageContainerProps> = ({
   ...props
 }) => {
   return (
-    <div
-      className={`min-h-screen px-4 pt-6 pb-24 flex ${className}`}
-      {...props}
-    >
+    <Wrapper className={`px-4 pt-6 flex ${className}`} {...props}>
       <div className="w-full max-w-[1100px] mx-auto relative z-0 flex flex-col">
         {children}
       </div>
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  min-height: calc(100vh - 64px);
+`;
