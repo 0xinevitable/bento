f04338a8df8cba0f6ef4491cf169373ee26516f8
Junho Yeo <i@junho.io>
2022-07-17 18:21:17 +0900

    [web] Show zap logo over chart

--- packages/bento-web/src/dashboard/components/AssetRatioChart.tsx
+++ packages/bento-web/src/dashboard/components/AssetRatioChart.tsx
@@ -118,9 +118,10 @@ export const AssetRatioChart: React.FC<AssetRatioChartProps> = ({
         </PieChart>
       </ResponsiveContainer>
 
-      {/* <AvatarContainer>
-        <Avatar src="/assets/avatar.png" />
-      </AvatarContainer> */}
+      <AvatarContainer>
+        {/* <Avatar src="/assets/avatar.png" /> */}
+        <BentoZapImage />
+      </AvatarContainer>
     </ChartContainer>
   );
 };
@@ -134,4 +135,28 @@ const ChartContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+const AvatarContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  pointer-events: none;
+`;
+
+const BentoZapImage = styled.img.attrs({
+  src: '/assets/illusts/bento-zap.png',
+})`
+  width: 160px;
+  height: 160px;
+  user-select: none;
 `;
