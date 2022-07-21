3f47069f15c2121d707f5aba185dc799d7164772
Junho Yeo <i@junho.io>
2022-07-21 18:06:14 +0900

    Show OAuth error and remove query params if included

--- packages/bento-web/src/hooks/useSession.ts
+++ packages/bento-web/src/hooks/useSession.ts
@@ -1,9 +1,11 @@
 import { AuthChangeEvent, Session } from '@supabase/supabase-js';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
 import { sessionAtom } from '@/recoil/session';
 import { Supabase } from '@/utils/Supabase';
+import { toast } from '@/utils/toast';
 
 const handleAuthChange = async (
   event: AuthChangeEvent,
@@ -36,6 +38,31 @@ export const SessionManager: React.FC = () => {
       }
     });
   }, []);
+
+  const router = useRouter();
+  useEffect(() => {
+    if (!router.query) {
+      return;
+    }
+
+    if (router.query.error === 'server_error') {
+      const description =
+        typeof router.query.error_description === 'string'
+          ? router.query.error_description
+          : undefined;
+
+      toast({
+        type: 'error',
+        title: 'Server Error',
+        description,
+      });
+
+      const url = new URL(location.href);
+      url.searchParams.delete('error');
+      url.searchParams.delete('error_description');
+      history.replaceState(null, '', url.href);
+    }
+  }, [router.query]);
 
   useEffect(() => {
     if (!currentSession) {
