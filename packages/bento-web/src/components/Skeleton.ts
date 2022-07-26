7cc969137e72cb11770985280cfc5a5fb6dcfed5
Junho Yeo <i@junho.io>
2022-07-27 01:08:37 +0900

    Implement component `Skeleton`

---
+++ packages/bento-web/src/components/Skeleton.ts
@@ -0,0 +1,19 @@
+import styled, { keyframes } from 'styled-components';
+
+const shimmer = keyframes`
+  0% {
+    background-position: -400px 0;
+  }
+  100% {
+    background-position: 400px 0;
+  }
+`;
+
+export const Skeleton = styled.div`
+  position: relative;
+  background-color: #171717;
+  background-image: linear-gradient(to right, #171717, #3b3b3b, #171717);
+  background-repeat: no-repeat;
+  background-size: 200% 100%;
+  animation: 1s ease-in-out infinite forwards running ${shimmer};
+`;
