33f6d17f2c1aa9a621869a7dacd0f6ac371a71cb
Junho Yeo <i@junho.io>
2022-08-04 20:26:47 +0900

    Add illustrations

--- packages/bento-web/src/landing/components/StatueQuoSection.tsx
+++ packages/bento-web/src/landing/components/StatueQuoSection.tsx
@@ -1,6 +1,18 @@
+import dedent from 'dedent';
 import styled from 'styled-components';
 
 import { Badge } from '@/components/Badge';
+
+const ASSETS = {
+  KING: [
+    '/assets/landing/status-quo-king.png',
+    '/assets/landing/status-quo-king@2x.png',
+  ],
+  WALLET: [
+    '/assets/landing/status-quo-wallet.png',
+    '/assets/landing/status-quo-wallet@2x.png',
+  ],
+};
 
 export const StatueQuoSection: React.FC = () => {
   return (
@@ -19,6 +31,30 @@ export const StatueQuoSection: React.FC = () => {
           remain in one address or chain.
         </Paragraph>
       </Information>
+
+      <IllustWrapper>
+        <IllustContainer>
+          <ChessKingIllustContainer>
+            <ChessKingIllust
+              src={ASSETS.KING[0]}
+              srcSet={dedent`
+                ${ASSETS.KING[0]} 1x,
+                ${ASSETS.KING[1]} 2x
+              `}
+            />
+          </ChessKingIllustContainer>
+          <InequalSymbol src="/assets/landing/inequal.svg" />
+          <WalletIllustContainer>
+            <WalletIllust
+              src={ASSETS.WALLET[0]}
+              srcSet={dedent`
+                ${ASSETS.WALLET[0]} 1x,
+                ${ASSETS.WALLET[1]} 2x
+              `}
+            />
+          </WalletIllustContainer>
+        </IllustContainer>
+      </IllustWrapper>
     </Section>
   );
 };
@@ -53,4 +89,68 @@ const Paragraph = styled.p`
   line-height: 145%;
   letter-spacing: 0.01em;
   color: #ffffff;
+`;
+
+const IllustWrapper = styled.div`
+  position: absolute;
+  top: -79px;
+  right: ${-110 - 38.08}px;
+`;
+const IllustContainer = styled.div`
+  position: relative;
+  width: 804.92px;
+  height: 513px;
+  z-index: 0;
+`;
+
+const CHESS_KING_BLUR_TOP = 140 - 52.9;
+const CHESS_KING_BLUR_LEFT = 140 - 20.82;
+const CHESS_KING_BLUR_RIGHT = 140 - 19.41;
+const CHESS_KING_BLUR_BOTTOM = 140 + 11.76;
+const ChessKingIllustContainer = styled.div`
+  width: 443.09px;
+  height: 455.76px;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+const ChessKingIllust = styled.img`
+  width: ${443.09 + CHESS_KING_BLUR_TOP + CHESS_KING_BLUR_BOTTOM}px;
+  height: ${455.76 + CHESS_KING_BLUR_LEFT + CHESS_KING_BLUR_RIGHT}px;
+
+  margin-top: ${-CHESS_KING_BLUR_TOP}px;
+  margin-left: ${-CHESS_KING_BLUR_LEFT}px;
+  margin-right: ${-CHESS_KING_BLUR_RIGHT}px;
+  margin-bottom: ${-CHESS_KING_BLUR_BOTTOM}px;
+`;
+
+const WALLET_ILLUST_BLUR_TOP = 140 - 98.51;
+const WALLET_ILLUST_BLUR_RIGHT = 140 - 30.28;
+const WalletIllustContainer = styled.div`
+  width: 355.5px;
+  height: 345px;
+
+  position: absolute;
+  top: 168px;
+  right: 0;
+  z-index: 1;
+`;
+const WalletIllust = styled.img`
+  width: ${355.5 + WALLET_ILLUST_BLUR_RIGHT}px;
+  height: ${345 + WALLET_ILLUST_BLUR_TOP}px;
+
+  margin-top: ${-WALLET_ILLUST_BLUR_TOP}px;
+  margin-right: ${-WALLET_ILLUST_BLUR_RIGHT}px;
+  z-index: 0;
+`;
+
+const InequalSymbol = styled.img`
+  width: 141px;
+  height: 152.29px;
+
+  position: absolute;
+  top: 181px;
+  left: 358px;
+  z-index: -1;
 `;
