27eb14a605599453f27b63f6b603069251e9aeaa
Junho Yeo <i@junho.io>
2022-08-09 14:58:28 +0900

    Use `translate3d` in animations for better rendering

--- packages/bento-web/src/landing/sections/HeaderSection.tsx
+++ packages/bento-web/src/landing/sections/HeaderSection.tsx
@@ -15,8 +15,8 @@ const ASSETS = {
 };
 
 const float = (y: number, reverse: boolean = false) => ({
-  initial: { y: !reverse ? -y : y },
-  animate: { y: !reverse ? y : -y },
+  initial: { transform: `translate3d(0, ${!reverse ? -y : y}px, 0)` },
+  animate: { transform: `translate3d(0, ${!reverse ? y : -y}px, 0)` },
   transition: {
     ease: 'linear',
     repeat: Infinity,
