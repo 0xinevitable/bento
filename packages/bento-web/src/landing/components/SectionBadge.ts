038c4a16290215977a2a646aea187d9bf4de1412
Junho Yeo <i@junho.io>
2022-08-09 13:35:26 +0900

    Mobile responsiveness for `SectionBadge`

--- packages/bento-web/src/landing/components/SectionBadge.ts
+++ packages/bento-web/src/landing/components/SectionBadge.ts
@@ -5,4 +5,8 @@ import { Badge } from '@/components/Badge';
 export const SectionBadge = styled(Badge)`
   background-color: #1a1a1a;
   backdrop-filter: none;
+
+  @media screen and (max-width: 600px) {
+    font-size: 12px;
+  }
 `;
