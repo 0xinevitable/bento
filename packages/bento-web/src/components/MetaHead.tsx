c7347bd74b66f06859a530288b06b73042a28503
Junho Yeo <i@junho.io>
2022-08-13 04:59:15 +0900

    Update meta tag for website description

--- packages/bento-web/src/components/MetaHead.tsx
+++ packages/bento-web/src/components/MetaHead.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 const meta = {
   title: 'Bento - Web3 Dashboard',
   description:
-    'The open-source, multi-chain web3 dashboard. Track every asset you own, regardless of chains and types.',
+    'The open-source, cross-chain web3 dashboard. Add multiple wallets, group your crypto—native tokens, DAOs, and NFTs—into one, and keep track of your favorite DeFi investments.',
   image: 'https://bento.finance/assets/og-image.png',
   url: 'https://bento.finance',
 };
