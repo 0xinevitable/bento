70e134ccfad85afe3856f49676a61414db00a254
savvyloch <jihojiho2003@gmail.com>
2022-08-14 19:10:26 +0900

    web: Remove not needed `React.Fragment`

--- packages/bento-web/src/profile/ProfileDetailPage/components/ProfileEditButton.tsx
+++ packages/bento-web/src/profile/ProfileDetailPage/components/ProfileEditButton.tsx
@@ -4,6 +4,6 @@ type ToggleStateProp = {
 
 export const ProfileEditButton: React.FC<ToggleStateProp> = ({ isEditing }) => (
   <div className="w-fit p-1 px-3 text-slate-100/75 border border-slate-100/75 rounded-2xl absolute top-[-24px] right-6 hover:opacity-50 transition-all">
-    {!isEditing ? <>Edit Profile</> : <>Submit</>}
+    {!isEditing ? 'Edit Profile' : 'Submit'}
   </div>
 );
