46a78868dfed17c71078419d3b6e6c8ba3c5969b
Junho Yeo <i@junho.io>
2022-10-04 14:49:19 +0900

    [web] Detach component `AnimatedTab` from profile feature

---
+++ packages/bento-web/src/components/AnimatedTab.tsx
@@ -0,0 +1,24 @@
+import { HTMLMotionProps, motion } from 'framer-motion';
+
+type AnimatedTabProps = {
+  selected: boolean;
+};
+export const AnimatedTab = (
+  props: AnimatedTabProps & HTMLMotionProps<'div'>,
+) => (
+  <motion.div
+    animate={
+      !props.selected
+        ? { opacity: 0, transform: 'scale(0.9)' }
+        : { opacity: 1, transform: 'scale(1)' }
+    }
+    style={{
+      originY: 0,
+      display: !props.selected ? 'none' : 'flex',
+      flexDirection: 'column',
+      ...props.style,
+    }}
+    transition={{ duration: 0.35 }}
+    {...props}
+  />
+);
