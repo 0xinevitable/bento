e53318b6b56379924452150d8eb049acda63d734
Junho Yeo <i@junho.io>
2022-10-07 12:52:25 +0900

    [web/hotfix] Include other params in `onClickSetAsProfile`

--- packages/bento-web/src/dashboard/sections/NFTListSection.tsx
+++ packages/bento-web/src/dashboard/sections/NFTListSection.tsx
@@ -55,47 +55,52 @@ export const NFTListSection: React.FC<Props> = ({
     });
   }, [selectedNFT, isMyProfile, profile]);
 
-  const onClickSetAsProfile = useCallback(async (assetImage: string) => {
-    try {
-      await axios.post(`/api/profile`, {
-        images: [assetImage],
-      });
-      revalidateProfile?.();
-
-      setTimeout(() => {
-        toast({
-          type: 'success',
-          title: 'Changes Saved',
+  const onClickSetAsProfile = useCallback(
+    async (assetImage: string) => {
+      try {
+        await axios.post(`/api/profile`, {
+          username: profile?.username.toLowerCase(),
+          display_name: profile?.display_name,
+          images: [assetImage],
         });
+        revalidateProfile?.();
 
-        document.body.scrollIntoView({
-          behavior: 'smooth',
+        setTimeout(() => {
+          toast({
+            type: 'success',
+            title: 'Changes Saved',
+          });
+
+          document.body.scrollIntoView({
+            behavior: 'smooth',
+          });
         });
-      });
-    } catch (e) {
-      if (e instanceof AxiosError) {
-        const errorResponse = e.response?.data as ErrorResponse;
-        if (errorResponse?.code === 'USERNAME_UNUSABLE') {
-          toast({
-            type: 'error',
-            title: errorResponse.message,
-            description: 'Please choose another username',
-          });
-        } else if (errorResponse?.code === 'VALUE_REQUIRED') {
-          toast({
-            type: 'error',
-            title: errorResponse.message,
-          });
-        } else {
-          toast({
-            type: 'error',
-            title: 'Server Error',
-            description: errorResponse?.message || 'Something went wrong',
-          });
+      } catch (e) {
+        if (e instanceof AxiosError) {
+          const errorResponse = e.response?.data as ErrorResponse;
+          if (errorResponse?.code === 'USERNAME_UNUSABLE') {
+            toast({
+              type: 'error',
+              title: errorResponse.message,
+              description: 'Please choose another username',
+            });
+          } else if (errorResponse?.code === 'VALUE_REQUIRED') {
+            toast({
+              type: 'error',
+              title: errorResponse.message,
+            });
+          } else {
+            toast({
+              type: 'error',
+              title: 'Server Error',
+              description: errorResponse?.message || 'Something went wrong',
+            });
+          }
         }
       }
-    }
-  }, []);
+    },
+    [profile, revalidateProfile],
+  );
 
   return (
     <AssetList>
