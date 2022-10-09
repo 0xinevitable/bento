064bceb9b7701458c8efac0f59f38c1252bb8bca
Junho Yeo <i@junho.io>
2022-10-09 22:54:07 +0900

    [web] Use `Promise.allSettled` in `asyncFlatMap`

--- packages/bento-web/src/utils/asyncFlatMap.ts
+++ packages/bento-web/src/utils/asyncFlatMap.ts
@@ -2,6 +2,11 @@ export const asyncFlatMap = async <T extends any, U extends any>(
   array: T[],
   callback: (value: T, index: number, array: T[]) => Promise<[] | U | U[]>,
 ): Promise<U[]> => {
-  const result = await Promise.all(array.map(callback));
-  return result.flat() as U[];
+  const result = await Promise.allSettled(array.map(callback));
+  return result.flatMap((res) => {
+    if (res.status === 'fulfilled') {
+      return res.value;
+    }
+    return [];
+  });
 };
