77867addf7e5a0046182f4e89ee90e98673f8053
Junho Yeo <i@junho.io>
2022-10-09 22:42:51 +0900

    Response with KlayStation delegations as `DeFiStaking`

--- packages/bento-web/pages/api/defis/klaytn/[walletAddress].ts
+++ packages/bento-web/pages/api/defis/klaytn/[walletAddress].ts
@@ -9,6 +9,7 @@ import {
   KOKONUTSWAP_LP_POOLS,
   klaytnChain,
 } from '@/defi/constants';
+import { KlayStation } from '@/defi/klaystation';
 import { KlaySwap } from '@/defi/klayswap';
 import { KokonutSwap } from '@/defi/kokonutswap';
 import { DeFiStaking } from '@/defi/types/staking';
@@ -57,71 +58,83 @@ const handler = async (req: APIRequest, res: NextApiResponse) => {
     KlaySwap.getLeveragePoolList().catch(() => undefined),
   ]);
 
-  const stakings: DeFiStaking[] = await asyncFlatMap(
-    tokenBalances,
-    async (token) => {
-      if (token.balance === null) {
-        // Indexed at least once
-        return [];
-      }
-
-      // KLAYswap LP
-      const klayswapLPPool = KLAYSWAP_LP_POOLS.find((v) =>
-        isSameAddress(v.exchange_address, token.contract_address),
-      );
-      if (!!klayswapLPPool) {
-        return KlaySwap.getLPPoolBalance(
-          walletAddress,
-          token.balance,
-          klayswapLPPool,
-        );
-      }
-
-      // KLAYswap Leverage Pool (Single Staking)
-      const klayswapLeveragePool = KLAYSWAP_LEVERAGE_POOLS.find((v) =>
-        isSameAddress(v.address, token.contract_address),
-      );
-      if (!!klayswapLeveragePool) {
-        return KlaySwap.getSinglePoolBalance(
-          walletAddress,
-          token.balance,
-          klayswapLeveragePool,
-          dynamicLeveragePools?.find(
-            (v) => v.address === klayswapLeveragePool.address,
-          ),
-        );
-      }
-
-      // KLAYswap Governance
-      if (isSameAddress(token.contract_address, KlaySwap.VOTING_KSP_ADDRESS)) {
-        return KlaySwap.getGovernanceStake(walletAddress, token.balance);
-      }
-
-      // Kokonutswap LP
-      const kokonutswapLPPool = KOKONUTSWAP_LP_POOLS.find((v) =>
-        isSameAddress(v.lpTokenAddress, token.contract_address),
-      );
-      if (!!kokonutswapLPPool) {
-        return KokonutSwap.getLPPoolBalance(
-          walletAddress,
-          token.balance,
-          kokonutswapLPPool,
-          KOKONUTSWAP_LP_POOLS,
-        );
-      }
-
-      // Kokonutswap Governance
-      if (
-        isSameAddress(token.contract_address, KokonutSwap.STAKED_KOKOS_ADDRESS)
-      ) {
-        return KokonutSwap.getGovernanceStake(walletAddress, token.balance);
-      }
-
+  const promisesForStakings = asyncFlatMap(tokenBalances, async (token) => {
+    if (token.balance === null) {
+      // Indexed at least once
       return [];
-    },
-  );
+    }
+
+    // KLAYswap LP
+    const klayswapLPPool = KLAYSWAP_LP_POOLS.find((v) =>
+      isSameAddress(v.exchange_address, token.contract_address),
+    );
+    if (!!klayswapLPPool) {
+      return KlaySwap.getLPPoolBalance(
+        walletAddress,
+        token.balance,
+        klayswapLPPool,
+      );
+    }
+
+    // KLAYswap Leverage Pool (Single Staking)
+    const klayswapLeveragePool = KLAYSWAP_LEVERAGE_POOLS.find((v) =>
+      isSameAddress(v.address, token.contract_address),
+    );
+    if (!!klayswapLeveragePool) {
+      return KlaySwap.getSinglePoolBalance(
+        walletAddress,
+        token.balance,
+        klayswapLeveragePool,
+        dynamicLeveragePools?.find(
+          (v) => v.address === klayswapLeveragePool.address,
+        ),
+      );
+    }
+
+    // KLAYswap Governance
+    if (isSameAddress(token.contract_address, KlaySwap.VOTING_KSP_ADDRESS)) {
+      return KlaySwap.getGovernanceStake(walletAddress, token.balance);
+    }
+
+    // Kokonutswap LP
+    const kokonutswapLPPool = KOKONUTSWAP_LP_POOLS.find((v) =>
+      isSameAddress(v.lpTokenAddress, token.contract_address),
+    );
+    if (!!kokonutswapLPPool) {
+      return KokonutSwap.getLPPoolBalance(
+        walletAddress,
+        token.balance,
+        kokonutswapLPPool,
+        KOKONUTSWAP_LP_POOLS,
+      );
+    }
+
+    // Kokonutswap Governance
+    if (
+      isSameAddress(token.contract_address, KokonutSwap.STAKED_KOKOS_ADDRESS)
+    ) {
+      return KokonutSwap.getGovernanceStake(walletAddress, token.balance);
+    }
+
+    return [];
+  });
+
+  // Delegations (KlayStation)
+  const promisesForDelegations = KlayStation.getDelegations(walletAddress);
+
+  const stakings = (
+    await Promise.all([
+      promisesForStakings.catch(handleError),
+      promisesForDelegations.catch(handleError),
+    ])
+  ).flat();
 
   res.status(200).json(stakings);
 };
 
 export default withCORS(handler);
+
+const handleError = (error: any) => {
+  console.error(error);
+  return [];
+};
