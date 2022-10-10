a08f2bbb466f42e635a683a760a0b6821b2a53ab
Junho Yeo <i@junho.io>
2022-10-10 19:18:09 +0900

    Style & Add validator badges

--- packages/bento-web/src/dashboard/components/CollapsePanel.tsx
+++ packages/bento-web/src/dashboard/components/CollapsePanel.tsx
@@ -27,16 +27,21 @@ export const CollapsePanel: React.FC<CollapsePanelProps> = ({
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 4px;
   width: 100%;
+
+  background: ${Colors.gray800};
+  border: 1px solid ${Colors.gray600};
+  border-radius: 8px;
 `;
 
 const Header = styled.button`
   color: ${Colors.gray100};
   display: flex;
-  background: ${Colors.gray900};
-  border: 1px solid ${Colors.gray800};
-  border-radius: 8px;
-  font-size: 22px;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 16px 14px;
 `;
-const Content = styled.div``;
+const Content = styled.div`
+  padding: 4px 14px 14px;
+  border-top: 1px solid ${Colors.gray600};
+`;
--- packages/bento-web/src/dashboard/components/DeFiStakingItem.tsx
+++ packages/bento-web/src/dashboard/components/DeFiStakingItem.tsx
@@ -1,7 +1,8 @@
-import { useTranslation } from 'next-i18next';
+import { Trans, useTranslation } from 'next-i18next';
 import styled from 'styled-components';
 
 import { DeFiStaking } from '@/defi/types/staking';
+import { Colors } from '@/styles';
 
 type DeFiStakingItemProps = {
   protocol: DeFiStaking;
@@ -12,24 +13,60 @@ export const DeFiStakingItem: React.FC<DeFiStakingItemProps> = ({
   const { t } = useTranslation('dashboard');
 
   return (
-    <Container>
-      <Name>{t(`defi-type-${protocol.type}`)}</Name>
-      <TokenLogoList>
-        {protocol.tokens.map((token, index) => (
-          <TokenLogo
-            key={`token-${index}`}
-            alt={token?.name || token?.symbol || 'Unknown'}
-            src={token?.logo}
+    <Container style={{ color: 'white' }}>
+      <Header>
+        <TokenLogoList>
+          {protocol.tokens.map((token, index) => (
+            <TokenLogo
+              key={`token-${index}`}
+              alt={token?.name || token?.symbol || 'Unknown'}
+              src={token?.logo}
+            />
+          ))}
+        </TokenLogoList>
+        {/* <Name>{t(`defi-type-${protocol.type}`)}</Name> */}
+        <Name>
+          <Trans
+            t={t}
+            i18nKey={`defi-type-${protocol.type}`}
+            components={{
+              validator: <ValidatorBadge />,
+            }}
           />
-        ))}
-      </TokenLogoList>
+        </Name>
+      </Header>
 
+      <span>wallet</span>
+      {!protocol.wallet ? null : protocol.wallet === 'unavailable' ? (
+        t('unavailable')
+      ) : (
+        <span style={{ color: 'white', display: 'flex' }}>
+          {protocol.wallet.lpAmount} LP ({`$${protocol.wallet.value || '?'}`})
+          <br />
+          {!!protocol.wallet.tokenAmounts &&
+            Object.entries(protocol.wallet.tokenAmounts).map(
+              ([tokenContract, tokenAmount]) => {
+                const token = protocol.tokens.find(
+                  (token) =>
+                    !!token &&
+                    'address' in token &&
+                    token.address === tokenContract,
+                );
+                return (
+                  <span key={`token-${tokenContract}`}>
+                    {`${tokenAmount || 0} ${token?.symbol}`}
+                    &nbsp;
+                  </span>
+                );
+              },
+            )}
+        </span>
+      )}
+
+      <span>staked</span>
       <span style={{ color: 'white', display: 'flex' }}>
         {protocol.staked.lpAmount} LP ({`$${protocol.staked.value || '?'}`})
         <br />
-      </span>
-
-      <span style={{ color: 'white', display: 'flex' }}>
         {!!protocol.staked.tokenAmounts &&
           Object.entries(protocol.staked.tokenAmounts).map(
             ([tokenContract, tokenAmount]) => {
@@ -58,9 +95,40 @@ const Container = styled.li`
   flex-direction: column;
   padding: 8px 0;
 `;
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
 const Name = styled.h5`
+  display: inline-flex;
+  align-items: center;
+  gap: 6px;
+
   color: white;
-  font-size: 20px;
+  font-size: 18px;
+  font-weight: 500;
+`;
+const ValidatorBadge = styled.span`
+  width: fit-content;
+  padding: 4px 8px;
+  background: rgba(51, 9, 17, 0.88);
+  border: 1px solid #ff214a;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.44);
+  border-radius: 36px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 100%;
+
+  text-align: center;
+  letter-spacing: -0.5px;
+
+  color: #ff214a;
 `;
 
 const TokenLogoList = styled.div`
@@ -70,6 +138,8 @@ const TokenLogo = styled.img`
   width: 32px;
   min-width: 32px;
   height: 32px;
+  border: 1px solid rgba(0, 0, 0, 0.45);
+  border-radius: 50%;
 
   &:not(:first-of-type) {
     margin-left: -8px;
