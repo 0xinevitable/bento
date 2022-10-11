2b1880a00329388fd86e703009d121b96cf259e9
Junho Yeo <i@junho.io>
2022-10-11 15:29:11 +0900

    [web] Implement `ServerSupabase` to use in server-side without RLS (#302)

--- packages/bento-web/pages/api/auth/index.ts
+++ packages/bento-web/pages/api/auth/index.ts
@@ -1,8 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import { ServerSupabase as Supabase } from '@/utils/ServerSupabase';
 import { withCORS } from '@/utils/middlewares/withCORS';
-
-import { Supabase } from '@/utils';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
