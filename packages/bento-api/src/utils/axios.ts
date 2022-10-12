2619c83874b8356d18dcbb2c05e050c9b2a3c3ea
Junho Yeo <i@junho.io>
2022-10-12 20:44:47 +0900

    [api] Remove `baseURL` from axios client

--- packages/bento-api/src/utils/axios.ts
+++ packages/bento-api/src/utils/axios.ts
@@ -1,7 +1,3 @@
 import Axios from 'axios';
 
-import { Config } from './Config';
-
-export const axios = Axios.create({
-  baseURL: Config.MAIN_API_BASE_URL || undefined,
-});
+export const axios = Axios.create();
