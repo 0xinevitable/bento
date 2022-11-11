f6b8836546803e1dc8d192c1415f5ce79cf84621
Junho Yeo <i@junho.io>
2022-11-12 06:38:29 +0900

    [adapters] Add logo in Cosmos Hub's service info

--- adapters/cosmos-hub/cosmos-hub/index.ts
+++ adapters/cosmos-hub/cosmos-hub/index.ts
@@ -3,7 +3,7 @@ import { ServiceInfo } from '@/_lib/types';
 const info: ServiceInfo = {
   native: true,
   name: 'Cosmos Hub',
-  logo: '',
+  logo: '/assets/icons/cosmos-hub.png',
   url: 'https://cosmos.network',
   description: 'The First of Thousands of Interconnected Blockchains',
 };
