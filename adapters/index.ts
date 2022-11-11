8c6e0e9308469312f225c0a786a8833b09a656fa
Junho Yeo <i@junho.io>
2022-11-11 14:07:56 +0900

    draft

--- adapters/index.ts
+++ adapters/index.ts
@@ -1,2 +1,45 @@
-export { type BentoChainAdapter } from './_lib/types';
+import {
+  BentoChainAdapter,
+  BentoProtocolAdapter,
+  BentoServiceAdapter,
+} from './_lib/types';
+
+export { type BentoChainAdapter };
 export { type CosmosSDKBasedChain } from './_lib/types/cosmos-sdk';
+
+type NetworkName = 'osmosis';
+
+type Adapters = Record<
+  NetworkName,
+  {
+    chain: BentoChainAdapter;
+    services: Record<
+      string,
+      {
+        info: BentoServiceAdapter;
+        protocols: Record<string, BentoProtocolAdapter>;
+      }
+    >;
+  }
+>;
+
+export const adapters: Adapters = {
+  osmosis: {
+    chain: require('./osmosis'),
+    services: {
+      osmosis: {
+        info: require('./osmosis/osmosis'),
+        protocols: {
+          governance: require('./osmosis/governance'),
+          gamm: require('./osmosis/gamm'),
+        },
+      },
+      'ion-dao': {
+        info: require('./osmosis/ion-dao'),
+        protocols: {
+          governance: require('./osmosis/ion-dao-governance'),
+        },
+      },
+    },
+  },
+};
