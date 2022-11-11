c131eadcdefc526f02393a273a2780cf2038c1d2
Junho Yeo <i@junho.io>
2022-11-12 06:39:29 +0900

    [adapters] Add Korean translations for Cosmos Hub

--- adapters/cosmos-hub/cosmos-hub/index.ts
+++ adapters/cosmos-hub/cosmos-hub/index.ts
@@ -2,9 +2,15 @@ import { ServiceInfo } from '@/_lib/types';
 
 const info: ServiceInfo = {
   native: true,
-  name: 'Cosmos Hub',
+  name: {
+    en: 'Cosmos Hub',
+    ko: '코스모스 허브',
+  },
   logo: '/assets/icons/cosmos-hub.png',
   url: 'https://cosmos.network',
-  description: 'The First of Thousands of Interconnected Blockchains',
+  description: {
+    en: 'The First of Thousands of Interconnected Blockchains',
+    ko: '코스모스 네트워크 최초의 퍼블릭 블록체인',
+  },
 };
 export default info;
