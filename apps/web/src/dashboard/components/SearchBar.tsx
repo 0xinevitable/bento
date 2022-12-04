f54b880ffbda0c9b2ac028dd5fa70ca58c338dbe
Allen Lopez <alllzxz@proton.me>
2022-12-04 19:51:08 +0900

    [web] Implement search

--- apps/web/src/dashboard/components/SearchBar.tsx
+++ apps/web/src/dashboard/components/SearchBar.tsx
@@ -1,11 +1,45 @@
 import styled from '@emotion/styled';
 import { Input } from '@geist-ui/core';
-import React from 'react';
+import { useCallback, useState } from 'react';
+
+import { useLazyEffect } from '@/hooks/useLazyEffect';
 
 export const SearchBar: React.FC = () => {
-  return <StyledInput placeholder="Search Accounts" />;
+  const [queryDraft, setQueryDraft] = useState<string>('');
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setQueryDraft(e.target.value);
+    },
+    [setQueryDraft],
+  );
+
+  const search = useCallback(async (value: string) => {
+    const { identifyWalletAddress } = await import('@bento/core');
+    const result = identifyWalletAddress(value);
+    console.log({ result });
+  }, []);
+
+  useLazyEffect(
+    () => {
+      search(queryDraft);
+    },
+    [search, queryDraft],
+    500,
+  );
+
+  return (
+    <StyledInput
+      className="sys"
+      placeholder="Search Accounts"
+      onChange={onChange}
+    />
+  );
 };
 
 const StyledInput = styled(Input)`
-  width: 100%;
+  &&& {
+    max-width: 400px;
+    width: 100%;
+  }
 `;
